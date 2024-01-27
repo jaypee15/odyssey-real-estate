92d28dd869c84e7dfd45aa515e523ef11b746b5e
jaypee15 <okoyejohnpaul15@gmail.com>
2024-01-27 07:36:00 +0100

    fix: add psycopg2

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsu
+postgres://odyssey_real_estate_user:SKTrGpBbeQkr6qPUiZQCDLZyJxF5b5vN@dpg-cmqa02i1hbls73ficao0-a/odyssey_real_estate
