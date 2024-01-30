2b07023a2f3b61c043ee2349ee7c91ff6f1ced36
jaypee15 <okoyejohnpaul15@gmail.com>
2024-01-30 21:53:08 +0100

    fix: migrate model changes

--- core/settings.py
+++ core/settings.py
@@ -183,3 +183,27 @@ ACCOUNT_FORMS = {"signup": "accounts.forms.UserSignupForm"}
 SOCIALACCOUNT_ADAPTER = "accounts.adapters.SocialAccountAdapter"
 # https://docs.allauth.org/en/latest/socialaccount/configuration.html
 SOCIALACCOUNT_FORMS = {"signup": "accounts.forms.UserSocialSignupForm"}
+
+
+# LOGGING
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# See https://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
--- listings/management/commands/update_properties.py
+++ listings/management/commands/update_properties.py
@@ -1,17 +1,21 @@
 import requests
+import logging
 from django.core.management.base import BaseCommand
 from listings.models import Property
 import xml.etree.ElementTree as ET
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = "Updates the properties from XML API"
 
     def handle(self, *args, **kwargs):
-        # Fetch XML data from the API
-        xml_url = "https://api.odysseydubaire.com/listings/feed/generic"
         try:
+            # Fetch XML data from the API
+            xml_url = "https://api.odysseydubaire.com/listings/feed/generic"
             response = requests.get(xml_url)
+            response.raise_for_status()  # Check for HTTP errors
+
             xml_data = response.text
 
             # Parse the XML data
@@ -19,71 +23,60 @@ class Command(BaseCommand):
 
             # Iterate over the property elements
             for property_element in root.findall("property"):
-               
-                property_data = {
-                    "date": property_element.find("date").text,
-                    "property_purpose": property_element.find("property_purpose").text,
-                    "ref": property_element.find("ref").text,
-                    "price": property_element.find("price").text,
-                    "currency": property_element.find("currency").text,
-                    "price_freq": property_element.find("price_freq").text,
-                    "type": property_element.find("type").text,
-                    "sub_location": property_element.find("sub_location").text,
-                    "town": property_element.find("town").text,
-                    "province": property_element.find("province").text,
-                    "country": property_element.find("country").text,
-                    "latitude": property_element.find("location/latitude").text,
-                    "longitude": property_element.find("location/longitude").text,
-                    "location_detail": property_element.find("location_detail").text,
-                    "title": property_element.find("title").text,
-                    "beds": property_element.find("beds").text,
-                    "baths": property_element.find("baths").text,
-                    "built_area": property_element.find("surface_area/built").text,
-                    "plot_area": property_element.find("surface_area/plot").text,
-                    "video_url": property_element.find("video_url").text,
-                    "description": property_element.find("desc").text,
-                    "agent_name": property_element.find("agent/name").text,
-                    "agent_mobile": property_element.find("agent/mobile").text,
-                    "agent_email": property_element.find("agent/email").text,
-                    "agent_reference": property_element.find("agent/reference").text,
-                    "agent_profile_picture": property_element.find(
-                        "agent/profile_picture"
-                    ).text,
-                    "image": [
-                        {
-                            "url": image_element.find("url").text,
-                        }
-                        for image_element in property_element.findall("images/image")
-                    ],
-                    "features": [
-                        {
-                            "feature": feature_element.text,
-                        }
-                        for feature_element in property_element.findall(
-                            "features/feature"
-                        )
-                    ],
-                    "amenities": [
-                        {
-                            "amenity": feature_element.text,
-                        }
-                        for feature_element in property_element.findall(
-                            "features/amenity"
-                        )
-                    ],
-                }
-                # print(property_data["amenities"])
+                try:
+                    # Extract property data from XML element
+                    property_data = self.extract_property_data(property_element)
 
-                # Create or update property instance
-                property_instance, created = Property.objects.get_or_create(
-                    ref=property_data["ref"], defaults=property_data
-                )
+                    # Create or update property instance
+                    property_instance, created = Property.objects.get_or_create(
+                        ref=property_data["ref"], defaults=property_data
+                    )
 
-                # If propert already exists, update its fields
-                if not created:
-                    print(f"Property {property_instance.ref} exists")
-                print(f"Property {property_instance.ref} created")
+                    # If property already exists, update its fields
+                    if not created:
+                        logger.info(f"Property {property_instance.ref} exists and updated")
+                    else:
+                        logger.info(f"Property {property_instance.ref} created")
+                except Exception as e:
+                    logger.error(f"Error processing property element: {e}")
 
         except requests.exceptions.RequestException as e:
-            print(f"Error fetching XML data: {e}")
+            logger.error(f"Error fetching XML data: {e}")
             return
+
+    def extract_property_data(self, property_element):
+        data = {
+            "date": property_element.findtext("date"),
+            "property_purpose": property_element.findtext("property_purpose"),
+            "ref": property_element.findtext("ref"),
+            "price": property_element.findtext("price"),
+            "currency": property_element.findtext("currency"),
+            "price_freq": property_element.findtext("price_freq"),
+            "type": property_element.findtext("type"),
+            "sub_location": property_element.findtext("sub_location"),
+            "town": property_element.findtext("town"),
+            "province": property_element.findtext("province"),
+            "country": property_element.findtext("country"),
+            "latitude": property_element.findtext("location/latitude"),
+            "longitude": property_element.findtext("location/longitude"),
+            "location_detail": property_element.findtext("location_detail"),
+            "title": property_element.findtext("title"),
+            "beds": (property_element.findtext("beds")),
+            "baths": (property_element.findtext("baths")),
+            "built_area": property_element.findtext("surface_area/built"),
+            "plot_area": property_element.findtext("surface_area/plot"),
+            "video_url": property_element.findtext("video_url"),
+            "description": property_element.findtext("desc"),
+            "agent_name": property_element.findtext("agent/name"),
+            "agent_mobile": property_element.findtext("agent/mobile"),
+            "agent_email": property_element.findtext("agent/email"),
+            "agent_reference": property_element.findtext("agent/reference"),
+            "agent_profile_picture": property_element.findtext("agent/profile_picture"),
+            "image": [
+                {"url": image_element.findtext("url")}
+                for image_element in property_element.findall("images/image")
+            ],
+            "features": [feature_element.text for feature_element in property_element.findall("features/feature")],
+            "amenities": [amenity_element.text for amenity_element in property_element.findall("amenities/amenity")],
+        }
+        return data
